5556ba9c4146a36b9ddf6c10522726082d7c42b6
Sam <samesch@gmail.com>
2016-03-30 06:09:00 -0500

    Completed Lesson 4 up to Products Section for Mean Stack Tutorial

---
+++ app/routes/categories.server.routes.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+module.exports = function(app) {
+	var categories = require('../../app/controllers/categories.server.controller');
+	var users = require('../../app/controllers/users.server.controller');
+
+	app.route('/categories')
+		.get(categories.list)
+		.post(users.requiresLogin, categories.create);
+
+	app.route('/categories/:categoryId')
+		.get(categories.read)
+		.put(users.requiresLogin, categories.update)
+		.delete(users.requiresLogin, categories.delete);
+
+	// Finish by binding the article middleware
+	// What's this? Where the categoryId is present in the URL
+	// the logic to 'get by id' is handled by this single function
+	// and added to the request object i.e. request.category.
+	app.param('categoryId', categories.categoryByID);
+};
